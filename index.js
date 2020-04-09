038dc3e3d5a80192074a00d46bec00841b20c276
kp1129 <katya.p.1129@gmail.com>
2020-04-09 11:21:05 -0700

    made port dynamic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./server");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
     console.log(`\n=== Server is listening on port ${port} ===\n`)
 });
